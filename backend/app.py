8903e9ba63e4d994d12bf31b8bb8fb1ae00051c1
Abin Antony <abin.antony@aot-technologies.com>
2021-03-10 18:46:31 -0800

    WIP

---
+++ backend/app.py
@@ -0,0 +1,122 @@
+
+import uuid
+from flask import Flask, g, jsonify, request
+from flask_oidc import OpenIDConnect
+from flask_cors import CORS
+
+# configuration
+DEBUG = True
+
+app = Flask(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+app.config.update({
+    'SECRET_KEY': 'foiclientapp',
+    'TESTING': True,
+    'DEBUG': True,
+    'OIDC_CLIENT_SECRETS': 'client_secrets.json',
+    'OIDC_ID_TOKEN_COOKIE_SECURE': False,
+    'OIDC_REQUIRE_VERIFIED_EMAIL': False,
+    'OIDC_USER_INFO_ENABLED': True,
+    'OIDC_SCOPES': ['openid', 'email', 'profile'],
+    'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post',
+    'OIDC_TOKEN_TYPE_HINT': 'access_token'
+})
+
+oidc = OpenIDConnect(app)
+
+
+
+BOOKS = [
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
+
+@app.route('/')
+def home():
+    if oidc.user_loggedin:
+        return("Hello,%s"%oidc.user_getfield('email'))
+    else:
+        return 'Welcome, please <a href="/dashboard">Login</a>'
+
+@app.route('/dashboard')
+@oidc.require_login
+def dashboard():
+    userinfo = oidc.user_getinfo(['email','preferred_username','sub'])
+
+    username = userinfo.get('preferred_username')
+    email  = userinfo.get('email')
+    userid = userinfo.get('sub')
+
+    return("This is your dashboard, %s and your email is %s! and UserId is %s"%(username,email,userid))
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
+
+
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+
+
+@app.route('/books/<book_id>', methods=['PUT', 'DELETE'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated!'
+    if request.method == 'DELETE':
+        remove_book(book_id)
+        response_object['message'] = 'Book removed!'
+    return jsonify(response_object)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000, debug=True)
