ecf9408bb9b083d70cada893834ca3f5c0240919
Abin Antony <abinjaik@gmail.com>
2021-03-23 11:11:50 -0700

    updated caunda deployment wf

--- bpmwf/deploywfmodel.sh
+++ bpmwf/deploywfmodel.sh
@@ -1,7 +1,1 @@
-curl -w "\n" \
--H "Accept: application/json" \
--F "deployment-name=foicc1" \
--F "enable-duplicate-filtering=true" \
--F "deploy-changed-only=true" \
--F "foisinglestageWF.bpmn=@/app/foisinglestageWF.bpmn" \
-http://localhost:8080/engine-rest/deployment/create
+curl -w -H "Accept: application/json" -F "deployment-name=foicc1" -F "enable-duplicate-filtering=true" -F "deploy-changed-only=true" -F "foisinglestageWF.bpmn=@/app/foisinglestageWF.bpmn" http://localhost:8080/engine-rest/deployment/create
